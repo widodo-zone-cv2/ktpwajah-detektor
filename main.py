d9c86a559a3538e577c9cd3650776aef553148d6
wahyuwidodo <sugengwahyuwidodo9@gmail.com>
2023-06-02 12:36:18 +0700

    perubahan code

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('hello')
+import numpy as np
+from ultralytics import YOLO
+import cv2
+import cvzone
+from PIL import Image, ImageTk
+import easyocr
+from tracker import *
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
